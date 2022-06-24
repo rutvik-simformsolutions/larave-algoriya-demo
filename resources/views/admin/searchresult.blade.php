f6905b8b8c1ff7a05d3e1ac2b34fdaae2e4821fc
= <rutvik.b@simformsolutions.com>
2022-06-24 20:03:45 +0530

    final

--- resources/views/admin/searchresult.blade.php
+++ resources/views/admin/searchresult.blade.php
@@ -7,12 +7,10 @@
                     <div class="row">
                         <div class="col">
                             <div>
-                                {{-- <div class="float-right">2021-04-20 04:04pm</div> --}}
-                                <h3>Lorem ipsum dolor sit amet</h3>
-                                <p class="mb-0">consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean
-                                    massa.
-                                    Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus
-                                    mus.</p>
+                                <p class="mb-0">Name : {!! $singlerecord['metadata'][$singlerecord['type'].'name']['value'] !!}</p>
+                                @if($singlerecord['type'] == 'skill')
+                                <p class="mb-0">{!! $singlerecord['metadata']['description']['value'] !!}</p>
+                                @endif
                             </div>
                         </div>
                     </div>
